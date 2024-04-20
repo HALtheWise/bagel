9b0d64d4f3350960dff05be3d1f0fc34b2fa2336
Eric Miller <eric@legoaces.org>
2024-04-19 23:21:42 -0700

    Tooling improvements

--- tools/go.sh
+++ tools/go.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # cd "$(dirname "$0")"
-exec bazel run --tool_tag=go --ui_event_filters=-info,-stdout,-stderr --noshow_progress -- @rules_go//go "${@}"
+exec bazel --output_base build/tools_data run --tool_tag=go --ui_event_filters=-info,-stdout,-stderr --noshow_progress -- @rules_go//go "${@}"
